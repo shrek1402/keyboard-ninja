78f7b11a6f18ca6793a90736466d2060079610a8
Митрохин Антон Игоревич <shrek.2000@yandex.ru>
2019-04-06 00:02:04 +0700

    09090909

--- keyboard/keyboard.cpp
+++ keyboard/keyboard.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     FileOpen mainMenu;
-    mainMenu.Open("keyboard/MainMenu.txt");
+    mainMenu.Open("MainMenu.txt");
     mainMenu.SetPositionX();
     mainMenu.SetPositionY(0);
     mainMenu.PrintMenu();
